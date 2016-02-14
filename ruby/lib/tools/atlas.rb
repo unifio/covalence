1b5434ba9a9426820ff0ffa599257d13a86ffac2
Wilson Carey <carey@unif.io>
2016-02-13 20:25:39 -0500

    Added Atlas tests

--- ruby/lib/tools/atlas.rb
+++ ruby/lib/tools/atlas.rb
@@ -8,30 +8,30 @@ module Atlas
   URL = "https://atlas.hashicorp.com"
 
   def self.reset_cache()
-    @cache = Hash.new{|h,k| h[k] = Hash.new}
+    @cache = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = Hash.new}}
   end
 
   reset_cache
 
-  def self.get_ami(name, region)
+  def self.get_artifact(slug, version, region)
     ensure_atlas_token_set
 
-    @cache[name][region] ||= begin
+    @cache[slug][version][region] ||= begin
       # Create and execute HTTP request
-      request = "#{URL}/api/v1/artifacts/#{name}/amazon.ami/search?version=latest&metadata.1.key=region.#{region}"
+      request = "#{URL}/api/v1/artifacts/#{name}/search?version=#{version}&metadata.1.key=region.#{region}"
       headers = {:'X-Atlas-Token' => ENV['ATLAS_TOKEN']}
 
       begin
         response = RestClient.get request, headers
       rescue RestClient::ExceptionWithResponse => err
-        fail "Unable to retrieve AMI ID for artifact '#{name}': " + err.message
+        fail "Unable to retrieve ID for artifact '#{name}': " + err.message
       end
 
       # Parse JSON response
       parsed = JSON.parse(response)
       latest = parsed["versions"].select {|version| version['metadata'].keys.include? "region.#{region}" }.first
 
-      # Return AMI for the region specified
+      # Return ID for the region specified
       if latest != nil
         latest["metadata"]["region.#{region}"]
       else
@@ -43,7 +43,7 @@ module Atlas
   def self.get_output(name, stack)
     ensure_atlas_token_set
 
-    @cache[name][stack] || begin
+    @cache[name][stack][0] || begin
       # Create and execute HTTP request
       request = "#{URL}/api/v1/terraform/state/#{stack}"
       headers = {:'X-Atlas-Token' => ENV['ATLAS_TOKEN']}
