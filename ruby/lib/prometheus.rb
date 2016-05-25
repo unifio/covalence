a90e9fc1a16bf72f230cc754377cc5c8290e5c9c
Wilson Carey <carey@unif.io>
2016-05-24 20:28:32 -0400

    Refactored rspec rake tasks and directory organization

--- ruby/lib/prometheus.rb
+++ ruby/lib/prometheus.rb
@@ -3,7 +3,6 @@ require "prometheus-unifio/version"
 module Prometheus
   WORKSPACE = File.expand_path(ENV['PROMETHEUS_WORKSPACE'] || '../../../', __FILE__)
   CONFIG = File.join(WORKSPACE, ENV['PROMETHEUS_CONFIG'] || 'prometheus.yaml')
-  RSPEC = File.join(WORKSPACE, ENV['PROMETHEUS_RSPEC_DIR'] || 'spec')
   PACKER = File.join(WORKSPACE, ENV['PROMETHEUS_PACKER_DIR'] || 'packer')
   TERRAFORM =  File.join(WORKSPACE, ENV['PROMETHEUS_TERRAFORM_DIR'] || 'terraform')
 end
