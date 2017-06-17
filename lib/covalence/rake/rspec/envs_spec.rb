011c6df5e429d025a485277a0eb7bd2fff1c6c6f
Wilson Carey <carey@unif.io>
2017-06-17 15:01:38 -0400

    Updated tests for use with either type of stack

--- lib/covalence/rake/rspec/envs_spec.rb
+++ lib/covalence/rake/rspec/envs_spec.rb
@@ -1,32 +1,23 @@
 require_relative '../../core/bootstrap'
 
 module Covalence
-  envs = EnvironmentRepository.all.select do |environ|
+  environments = EnvironmentRepository.find_all.select do |environ|
     TEST_ENVS.include?(environ.name.to_s)
   end
 
-  envs.each do |env|
-    env.stacks.each do |stack|
+  environments.each do |environment|
+    environment.stacks.each do |stack|
+      EnvironmentRepository.populate_stack(stack)
+      case stack.type
+      when 'terraform'
+        tf_tasks = TerraformStackTasks.new(stack)
+      when 'packer'
+        packer_tasks = PackerStackTasks.new(stack)
+      end
+
       path = File.expand_path(File.join(TERRAFORM, stack.module_path))
 
-      describe "Verify #{env.name}:#{stack.name}" do
-
-        before(:each) do
-          @tmp_dir = Dir.mktmpdir
-          # Copy module to the workspace
-          FileUtils.copy_entry path, @tmp_dir
-
-          # Copy any dependencies to the workspace
-          stack.dependencies.each do |dep|
-            Covalence::LOGGER.info "Copying '#{dep}' dependency to #{@tmp_dir}"
-            dep_path = File.expand_path(File.join(Covalence::TERRAFORM, dep))
-            FileUtils.cp_r dep_path, @tmp_dir
-          end
-        end
-
-        after(:each) do
-          FileUtils.remove_entry @tmp_dir
-        end
+      describe "Verify #{environment.name}:#{stack.name}" do
 
         if stack.type == 'terraform'
 
@@ -37,9 +28,17 @@ module Covalence
           end
 
           it 'passes validation' do
-            Dir.chdir(@tmp_dir) do
-              Covalence::LOGGER.info "In #{@tmp_dir}:"
+            tmp_dir = Dir.mktmpdir
+            # Copy module to the workspace
+            FileUtils.copy_entry path, tmp_dir
 
+            # Copy any dependencies to the workspace
+            stack.dependencies.each do |dep|
+              dep_path = File.expand_path(File.join(Covalence::TERRAFORM, dep))
+              FileUtils.cp_r dep_path, tmp_dir
+            end
+
+            Dir.chdir(tmp_dir) do
               TerraformCli.terraform_get(path)
               TerraformCli.terraform_init
 
@@ -50,34 +49,20 @@ module Covalence
           end
 
           it 'passes execution' do
-            Dir.chdir(@tmp_dir) do
-              Covalence::LOGGER.info "In #{@tmp_dir}:"
-
-              TerraformCli.terraform_get(path)
-              TerraformCli.terraform_init
-
-              stack.materialize_cmd_inputs
-              args = ["-input=false", stack.args, "-var-file=covalence-inputs.tfvars"].flatten.compact.reject(&:empty?).map(&:strip)
-
-              expect {
-                expect(TerraformCli.terraform_plan(args: args)).to be true
-              }.to_not raise_error
-            end
+            expect {
+              expect(tf_tasks.stack_verify).to be true
+            }.to_not raise_error
           end
 
         elsif stack.type == 'packer'
 
           it 'passes validation' do
-            pending
-          end
-
-          it 'passes execution' do
-            pending
+            expect {
+              expect(packer_tasks.context_validate(stack.contexts.first.to_packer_command_options)).to be true
+            }.to_not raise_error
           end
         end
-
       end
     end
   end
-
 end
