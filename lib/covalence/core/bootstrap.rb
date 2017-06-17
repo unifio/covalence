62e845a17d8bd3bf302ad6fedb26ece81e7aa3f7
Wilson Carey <carey@unif.io>
2017-06-17 15:01:38 -0400

    Updated tests for use with either type of stack

--- lib/covalence/core/bootstrap.rb
+++ lib/covalence/core/bootstrap.rb
@@ -5,3 +5,4 @@ require_relative '../../covalence'
 Dir[File.expand_path("cli_wrappers/**/*.rb", File.dirname(__FILE__))].sort.each { |file| require file }
 Dir[File.expand_path("entities/**/*.rb", File.dirname(__FILE__))].sort.each { |file| require file }
 Dir[File.expand_path("repositories/**/*.rb", File.dirname(__FILE__))].sort.each { |file| require file }
+Dir[File.expand_path("services/**/*.rb", File.dirname(__FILE__))].sort.each { |file| require file }
